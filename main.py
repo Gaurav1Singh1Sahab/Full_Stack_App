fdcaa44d380842b102781225e531b6224516cd23
Gaurav1Singh1Sahab <gs94gauravsingh@gmail.com>
2024-04-14 16:43:09 +0530

    Added till Inserting Data in DB

---
+++ main.py
@@ -0,0 +1,64 @@
+# app.py
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+
+app = Flask(__name__)
+DATABASE = 'data.db'
+
+
+def create_connection():
+    conn = None
+    try:
+        conn = sqlite3.connect(DATABASE)
+    except sqlite3.Error as e:
+        print(e)
+    return conn
+
+
+def create_table():
+    conn = create_connection()
+    if conn is not None:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS details (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                gender TEXT NOT NULL,
+                year_of_birth INTEGER NOT NULL
+            )
+        ''')
+        conn.commit()
+        conn.close()
+
+
+create_table()
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', name='Flask')
+
+
+@app.route('/add_details', methods=['GET', 'POST'])
+def add_details():
+    if request.method == 'POST':
+        name = request.form['name']
+        gender = request.form['gender']
+        year_of_birth = request.form['year_of_birth']
+
+        conn = create_connection()
+        if conn is not None:
+            cursor = conn.cursor()
+            cursor.execute('''
+                INSERT INTO details (name, gender, year_of_birth)
+                VALUES (?, ?, ?)
+            ''', (name, gender, year_of_birth))
+            conn.commit()
+            conn.close()
+
+        return redirect(url_for('index'))
+    return render_template('add_details.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
